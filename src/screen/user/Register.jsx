72ffc661c394fb743db8eb2a73562c30e6067904
steeven-js <kisama@gmail.com>
2023-12-19 17:51:02 -0400

    Add Firebase authentication and registration functionality

--- src/screen/user/Register.jsx
+++ src/screen/user/Register.jsx
@@ -1,15 +1,48 @@
-import { View, Text, Button } from 'react-native'
-import BackIcon from 'react-native-vector-icons/Ionicons';
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const Register = ({ navigation }) => {
-    return (
-        <View>
-            <BackIcon name="arrow-back" size={30} color="#000" onPress={() => navigation.navigate('Plantes médicinales')} />
-            <Text>Register</Text>
-            <Button title="Login" onPress={() => { navigation.navigate('LoginScreen') }} />
-        </View>
-    )
-}
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-export default Register
+    const handleSignUp = async () => {
+        try {
+            await auth().createUserWithEmailAndPassword(email, password);
+            // L'utilisateur est inscrit avec succès
+            console.log('User registered successfully!');
+        } catch (error) {
+            console.error('Error registering user:', error.message);
+        }
+    };
+
+    return (
+        <View 
+            style={{ 
+                flex: 1, 
+                justifyContent: 'center', 
+                alignItems: 'center' 
+            }}
+        >
+            <Text>Register</Text>
+            <TextInput
+                style={{ borderBottomWidth: 1, borderColor: 'black' }}
+                placeholder="Email"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+            />
+            <TextInput
+                style={{ borderBottomWidth: 1, borderColor: 'black' }}
+                placeholder="Password"
+                secureTextEntry
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+            />
+            <Button title="Register" onPress={handleSignUp} />
+            <Text>Already have an account? </Text>
+            <Text onPress={() => navigation.navigate('LoginScreen')}>Login</Text>
+        </View>
+    );
+};
+
+export default Register;
