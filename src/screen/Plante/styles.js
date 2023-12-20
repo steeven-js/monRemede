4bb63e482de8d2ebc9acfca050f9af42da3a5eb7
steeven-js <kisama@gmail.com>
2023-12-20 13:11:28 -0400

    Update code formatting and fix minor bugs

---
+++ src/screen/Plante/styles.js
@@ -0,0 +1,50 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+const numColumns = 2;
+const columnWidth = width / numColumns;
+
+export default StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+    },
+    overlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    },
+    container: {
+        backgroundColor: 'transparent',
+    },
+    spacing: {
+        color: 'white',
+    },
+    plant: {
+        width: columnWidth - 20,
+        height: columnWidth - 20,
+        borderWidth: 1,
+        borderColor: 'rgba(255, 255, 255, 0.6)',
+        borderRadius: 5,
+        marginBottom: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        margin: 10,
+    },
+    plantInfoContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5,
+        padding: 5,
+    },
+    plantName: {
+        color: 'white',
+        textAlign: 'center',
+        fontFamily: 'Dosis-Regular',
+    },
+});
