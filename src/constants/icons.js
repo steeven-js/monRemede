42ab65ece7f47908605bc09f02bb65a74f5c3eb0
steeven-js <kisama@gmail.com>
2023-12-27 22:58:23 -0400

    Update icons in TopBar component

---
+++ src/constants/icons.js
@@ -0,0 +1,9 @@
+import favoris from '../assets/icons/favoris.png';
+import plante from '../assets/icons/plante.png';
+import usage from '../assets/icons/usage.png';
+
+export default {
+    favoris,
+    plante,
+    usage,
+};
--- src/navigation/TopBar/index.jsx
+++ src/navigation/TopBar/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import HeartIcon from 'react-native-vector-icons/FontAwesome6';
-import StarIcon from 'react-native-vector-icons/FontAwesome6';
-import LeafIcon from 'react-native-vector-icons/FontAwesome';
 import SearchIcon from 'react-native-vector-icons/Ionicons';
 import MenuIcon from 'react-native-vector-icons/Ionicons';
+
+import { icons } from '../../constants';
+
 import styles from './styles';
 
 const TopBar = ({ navigation, route }) => {
@@ -31,13 +31,13 @@ const TopBar = ({ navigation, route }) => {
             {/* Onglets */}
             <View style={styles.container}>
                 <TouchableOpacity onPress={() => navigateToScreen('Usages thérapeutiques')} style={[styles.tab, route && route.name === 'Usages thérapeutiques' && styles.activeTab1]}>
-                    <Image source={require('../../assets/icons/usage.png')} style={styles.icon} />
+                    <Image source={icons.usage} style={styles.icon} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => navigateToScreen('Plantes médicinales')} style={[styles.tab, route && route.name === 'Plantes médicinales' && styles.activeTab2]}>
-                    <Image source={require('../../assets/icons/plante.png')} style={styles.icon} />
+                    <Image source={icons.plante} style={styles.icon} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => navigateToScreen('Favoris')} style={[styles.tab, route && route.name === 'Favoris' && styles.activeTab3]}>
-                    <Image source={require('../../assets/icons/favoris.png')} style={styles.icon} />
+                    <Image source={icons.favoris} style={styles.icon} />
                 </TouchableOpacity>
             </View>
         </LinearGradient>
