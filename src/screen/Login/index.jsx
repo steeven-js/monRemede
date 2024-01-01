ff5dc28d25c4d8351eb9b5e76b5da9c7ffcb3bf9
steeven-js <kisama@gmail.com>
2024-01-01 16:34:17 -0400

    GOOGLE AUTH FIREBASE

--- src/screen/Login/index.jsx
+++ src/screen/Login/index.jsx
@@ -3,6 +3,7 @@ import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'react-native-linear-gradient';
 import MenuIcon from 'react-native-vector-icons/Ionicons';
 import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 // Custom components
 import Button from '../../components/buttons/Button';
@@ -23,7 +24,20 @@ import { SvgXml } from 'react-native-svg';
 import OrDivider from '../../components/dividers/OrDivider';
 import Question from '../../components/paragraphs/Question';
 
+
 const Connexion = ({ navigation }) => {
+    GoogleSignin.configure({
+        scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+        webClientId: '62541315106-4lcmcu1rom9hbm6megrl9ldlu94rj5hk.apps.googleusercontent.com', // client ID of type WEB for your server. Required to get the idToken on the user object, and for offline access.
+        offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+        hostedDomain: '', // specifies a hosted domain restriction
+        forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+        accountName: '', // [Android] specifies an account name on the device that should be used
+        iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+        googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+        openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+        profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+    });
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [afficherMotDePasse, setAfficherMotDePasse] = useState(false);
@@ -79,6 +93,19 @@ const Connexion = ({ navigation }) => {
             setIsLoading(false);
         }
     };
+
+    const googleSignIn = async () => {
+        // Check if your device supports Google Play
+        await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+        // Get the users ID token
+        const { idToken } = await GoogleSignin.signIn();
+
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(googleCredential);
+    }
 
     return (
         <View style={[styles.mainWrapper, { backgroundColor: COLORS.accent }]}>
@@ -156,7 +183,7 @@ const Connexion = ({ navigation }) => {
 
                 <View style={styles.socialMediaIconsWrapper}>
                     <Animatable.View animation="bounceIn" delay={2100}>
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={googleSignIn}>
                             <SvgXml
                                 xml={ic_google}
                                 width={STANDARD_SOCIAL_ICON_SIZE}
--- src/screen/Register/index.jsx
+++ src/screen/Register/index.jsx
@@ -5,6 +5,10 @@ import LinearGradient from 'react-native-linear-gradient';
 import MenuIcon from 'react-native-vector-icons/Ionicons';
 import auth from '@react-native-firebase/auth';
 import { COLORS } from '../../config/Colors';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { SvgXml } from 'react-native-svg';
+import { STANDARD_SOCIAL_ICON_SIZE } from '../../config/Constants';
+import ic_google from '../../assets/icons/svg/ic_google';
 
 import styles from './styles';
 
@@ -15,8 +19,21 @@ import PasswordTextInput from '../../components/inputs/PasswordTextInput';
 import Question from '../../components/paragraphs/Question';
 import Link from '../../components/links/Link';
 import Button from '../../components/buttons/Button';
+import OrDivider from '../../components/dividers/OrDivider';
 
 const Register = ({ navigation }) => {
+    GoogleSignin.configure({
+        scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+        webClientId: '62541315106-4lcmcu1rom9hbm6megrl9ldlu94rj5hk.apps.googleusercontent.com', // client ID of type WEB for your server. Required to get the idToken on the user object, and for offline access.
+        offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+        hostedDomain: '', // specifies a hosted domain restriction
+        forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+        accountName: '', // [Android] specifies an account name on the device that should be used
+        iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+        googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+        openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+        profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+    });
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [afficherMotDePasse, setAfficherMotDePasse] = useState(false);
@@ -65,6 +82,19 @@ const Register = ({ navigation }) => {
         } finally {
             setIsLoading(false);
         }
+    };
+
+    const googleSignIn = async () => {
+        // Check if your device supports Google Play
+        await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+        // Get the users ID token
+        const { idToken } = await GoogleSignin.signIn();
+
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(googleCredential);
     };
 
     const checkUserLoggedIn = async () => {
@@ -160,6 +190,24 @@ const Register = ({ navigation }) => {
                                     />
                                     {emptyFieldsError ? <Text style={styles.errorMessage}>{emptyFieldsError}</Text> : null}
                                 </Animatable.View>
+
+                                <View style={styles.verticalSpacer} />
+
+                                <Animatable.View animation="fadeInUp" delay={1500}>
+                                    <OrDivider label="or login with" />
+                                </Animatable.View>
+
+                                <View style={styles.socialMediaIconsWrapper}>
+                                    <Animatable.View animation="bounceIn" delay={2100}>
+                                        <TouchableOpacity onPress={googleSignIn}>
+                                            <SvgXml
+                                                xml={ic_google}
+                                                width={STANDARD_SOCIAL_ICON_SIZE}
+                                                height={STANDARD_SOCIAL_ICON_SIZE}
+                                            />
+                                        </TouchableOpacity>
+                                    </Animatable.View>
+                                </View>
 
                                 <View style={styles.verticalSpacer} />
 
